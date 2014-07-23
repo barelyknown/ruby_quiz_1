2b315ddc229e5da1106d94a9ebab7883eb3ce8ab
Sean Devine <barelyknown@icloud.com>
2014-07-23 11:32:44 -0400

    change require

--- lib/ruby_quiz_1.rb
+++ lib/ruby_quiz_1.rb
@@ -1,12 +1,12 @@
-require_relative "ruby_quiz_1/version"
+require "ruby_quiz_1/version"
 
-require_relative "ruby_quiz_1/message"
-require_relative "ruby_quiz_1/character_group"
-require_relative "ruby_quiz_1/solitaire"
-require_relative "ruby_quiz_1/card"
-require_relative "ruby_quiz_1/joker"
-require_relative "ruby_quiz_1/suit"
-require_relative "ruby_quiz_1/deck"
+require "ruby_quiz_1/message"
+require "ruby_quiz_1/character_group"
+require "ruby_quiz_1/solitaire"
+require "ruby_quiz_1/card"
+require "ruby_quiz_1/joker"
+require "ruby_quiz_1/suit"
+require "ruby_quiz_1/deck"
 
 module RubyQuiz1
 
